68f4f1d6fbf38e934b9ba80dea1ab428b3ee0d67
mkraetke <mkraetke@gmx.de>
2015-08-20 14:05:03 +0200

    switch off auto-close for side nav

--- js/init.js
+++ js/init.js
@@ -68,7 +68,7 @@
     $('.button-collapse').sideNav({
         menuWidth: 240, // Default is 240
         edge: 'left', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
       }
     );
   $('select').not('.disabled').material_select();
